c084a28fe10a90049ca908633e49424e98c14395
shalini004 <77494427+shalini004@users.noreply.github.com>
2021-02-20 19:03:52 +0530

    Update P2_Heap Sort Algorithm.py

--- P2_Heap Sort Algorithm.py
+++ P2_Heap Sort Algorithm.py
@@ -1,47 +1,41 @@
-# To heapify subtree rooted at index i.
-# n is size of heap
-def heapify(arr, n, i):
-    largest = i # Initialize largest as root
-    l = 2 * i + 1 # left = 2*i + 1
-    r = 2 * i + 2 # right = 2*i + 2
+def heapify(arr,n,i):
+    largest=i
+    l=2*i+1
+    r=2*i+2
 
-    # See if left child of root exists and is
-    # greater than root
-    if l < n and arr[i] < arr[l]:
-        largest = l
-        
-    # See if right child of root exists and is
-    # greater than root
-    if r < n and arr[largest] < arr[r]:
-        largest = r
+    if l<n and arr[i]<arr[l]:
+        largest=l
 
-    # Change root, if needed
-    if largest != i:
-        arr[i],arr[largest] = arr[largest],arr[i] # swap
+    if r<n and arr[largest]<arr[r]:
+        largest=r
 
-        # Heapify the root.
-        heapify(arr, n, largest)
+    if largest !=i:
+        arr[i],arr[largest]=arr[largest],arr[i]
+        heapify(arr,n,largest)
 
-# The main function to sort an array of given size
 def heapSort(arr):
-    n = len(arr)
+    n=len(arr)
+    for i in range(n,-1,-1):
+        heapify(arr,n,i)
 
-    # Build a maxheap.
-    for i in range(n, -1, -1):
-        heapify(arr, n, i)
+    for i in range(n-1,0,-1):
+        arr[i],arr[0]=arr[0],arr[i]
+        heapify(arr,i,0)
 
-    # One by one extract elements
-    for i in range(n-1, 0, -1):
-        arr[i], arr[0] = arr[0], arr[i] # swap
-        heapify(arr, i, 0)
+arr=[12,11,13,5,6,7]
+heapSort(arr)
+n=len(arr)
+print("Sorted array is")
+for i in range(n):
+    print("%d" %arr[i])
+        
 
-
-arr = [11, 34, 9, 5, 16, 10] 
-n = len(arr) 
-print("Original array:\n") 
-for i in range(n):       
-    print("%d " % arr[i], end = '')
-heapSort(arr) 
-print("\n\nSorted array:\n") 
-for i in range(n):       
-    print("%d " % arr[i], end = '')
+output:
+Sorted array is
+5
+6
+7
+11
+12
+13
+> 
